406fb3a447d016c9ce36a001df3336f33ee49778
Salman Shaikh <salman_nj2_107@masai.school>
2021-12-25 19:55:20 +0530

    login page styles

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,5 +1,40 @@
+import background from "./background.mp4";
+import styles from "./Login.module.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 function Login() {
-  return <>login</>;
+  const [user, setuser] = useState("");
+  const [room, setroom] = useState("");
+  return (
+    <>
+      <div className={styles.container}>
+        <video autoPlay loop muted>
+          <source src={background} type="video/mp4" />
+        </video>
+        <div className={styles.form}>
+          <div>Chat World</div>
+          <form method="post">
+            <input
+              type="text"
+              placeholder="Enter your name..."
+              onChange={(e) => setuser(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Enter Room"
+              onChange={(e) => setroom(e.target.value)}
+            />
+            <Link
+              onClick={(e) => (!user || !room ? e.preventDefault() : null)}
+              to={`/chat?name=${user}&room=${room}`}
+            >
+              <input type="submit" value="Login" className={styles.submit} />
+            </Link>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
