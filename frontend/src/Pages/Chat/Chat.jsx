e52151fdab0bb398b8d71cde7d2ce1bef10c6fbd
Salman Shaikh <salman_nj2_107@masai.school>
2021-12-26 13:57:42 +0530

    finished functionality and almost done styling

--- frontend/src/Pages/Chat/Chat.jsx
+++ frontend/src/Pages/Chat/Chat.jsx
@@ -1,5 +1,8 @@
 import io from "socket.io-client";
 import React, { useEffect, useState } from "react";
+import "./Chat.css";
+import grp from "./grp.svg";
+import send1 from "./send1.svg";
 let socket;
 function Chat() {
   const [user, setUser] = useState();
@@ -34,7 +37,12 @@ function Chat() {
   useEffect(() => {
     socket.on("message", (msg) => {
       setMessages((prevMsg) => [...prevMsg, msg]);
+      setTimeout(() => {
+        var objDiv = document.querySelector(".chatbox");
+        objDiv.scrollTop = objDiv.scrollHeight;
+      }, 10);
     });
+
     socket.on("activeUsers", (user) => {
       setActiveUsers(user);
     });
@@ -43,37 +51,71 @@ function Chat() {
   const sendMessage = (e) => {
     // e.preventDefault();
     socket.emit("sendMsg", msg, () => setMsg(""));
+    setTimeout(() => {
+      var objDiv = document.querySelector(".chatbox");
+      objDiv.scrollTop = objDiv.scrollHeight;
+    }, 10);
   };
+  console.log("messages", messages);
   return (
     <>
-      <div>
-        <ul>
-          {activeUsers.map((user) => (
-            <li>{user.user}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <ul>
-          {messages.map((e, i) =>
-            e.user !== user?.toLowerCase() ? (
-              <li key={i} style={{ color: "red" }}>
-                {JSON.stringify(e)}
-              </li>
-            ) : (
-              <li key={i} style={{ color: "green" }}>
-                {JSON.stringify(e)}
-              </li>
-            )
-          )}
-        </ul>
-        <input
-          type="text"
-          value={msg}
-          onChange={(e) => setMsg(e.target.value)}
-          onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
-        />
-        <button onClick={(e) => sendMessage(e)}>send</button>
+      <div className="container">
+        <div className="activeuser">
+          <div>
+            Active Users <span> &#8659;</span>
+          </div>
+          <div>
+            {activeUsers.map((e) => (
+              <div>
+                <span>{e.user}</span>
+                <span className="dot"></span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className="room">
+            <img src={grp} alt="grp" />
+            <span>{room}</span>
+          </div>
+          <div className="chatbox">
+            {messages.map((e, i) =>
+              e.user !== user?.toLowerCase() ? (
+                <div className="chat left">
+                  <div>
+                    <p>{e.user}</p>
+                    <p>{e.date}</p>
+                  </div>
+                  <div>
+                    <p>{e.text}</p>
+                  </div>
+                </div>
+              ) : (
+                <div className="chat right">
+                  <div>
+                    <p>{e.user}</p>
+                    <p>{e.date}</p>
+                  </div>
+                  <div>
+                    <p>{e.text}</p>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+          <div className="inp">
+            <input
+              type="text"
+              placeholder="Send message..."
+              value={msg}
+              onChange={(e) => setMsg(e.target.value)}
+              onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
+            />
+            <button onClick={(e) => sendMessage(e)}>
+              <img src={send1} alt="send" />
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
